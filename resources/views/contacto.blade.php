422742c7ff77e2d832609698dda9275e12761fbe
alancarrillo <alang.carrillo@alumnos.ugd.mx>
2022-09-13 16:39:47 -0500

    inicio proyecto

---
+++ resources/views/contacto.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FORMULARIO</title>
+</head>
+<body> 
+    <!--tutulo-->
+    <div>
+        <H1>INGRESA DATOS</H1>
+    </div>
+    <!--termina el titulo-->
+    <!--inicia con el nombre-->
+    <div>
+        ingresa nombre <br>
+        <input type="text">
+    </div>
+    <!--termina el div del nombre-->
+    <!--inicia con correo-->
+    <div>
+        ingresa correo electronico <br>
+        <input type="email">
+    </div>
+    <!--termina correo-->
+    <DIv>
+        ingresa algun archivo<br>
+        <input type="file"> <br>
+        ingresa algun comentario<br>
+        <input type="text">
+    </DIv>
+</body>
+</html>
